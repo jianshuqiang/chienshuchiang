b413b67cf853c7af9b84e1d0b32600f68b3df0c4
jianshuqiang <30968516+jianshuqiang@users.noreply.github.com>
2019-03-28 21:47:57 +0800

    Update Solution_3_28.java

--- src/Solution_3_28.java
+++ src/Solution_3_28.java
@@ -9,14 +9,12 @@ public class Solution_3_28 {
         int l = 1, r = len - 2;
         while (l < r) {
             int m = l + ((r - l) >> 1);
-            if (A[m] > A[m - 1] && A[m] > A[m + 1]) {
-
-                return m;
-            } else if (A[m] < A[m - 1]) {
-                r = m - 1;
-            } else {
-                l = m + 1;
-            }
+            if (A[m] < A[m - 1]) 
+                r = m - 1;                
+            else if (A[m] > A[m + 1]) 
+                return m;                
+            else 
+                l = m + 1;            
         }
         return l;
     }

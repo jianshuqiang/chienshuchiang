a5d40500d1d0387953a9a4a928925e0fa29ddd43
jianshuqiang <30968516+jianshuqiang@users.noreply.github.com>
2019-03-29 21:41:33 +0800

    Update Solution_3_30.java

--- src/main/java/Solution_3_30.java
+++ src/main/java/Solution_3_30.java
@@ -14,14 +14,12 @@ public class Solution_3_30 {
         // write your code here
         int l = 1, r = n, m;
         while (l < r) {
-            System.out.println(l + "...." + r);
             m = l + ((r - l) >> 1);
             if (SVNRepo.isBadVersion(m))
                 r = m;
             else 
                 l = m + 1;
-        }
-        
+        }        
         return l;
     }
 }
